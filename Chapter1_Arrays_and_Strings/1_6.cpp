6becccb63ca90f1499b433b17e7e9788fb6d85a8
Hane Summer <haneyuzuru@yahoo.com>
2016-09-05 23:30:57 +0800

    1_6上传，需要注意n为奇数时需要避免重复移动

--- Chapter1_Arrays_and_Strings/1_6.cpp
+++ Chapter1_Arrays_and_Strings/1_6.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory.h>
 
 using namespace std;
 
@@ -15,16 +16,18 @@ int main(){
 
     for(int i = 0; i != N; i ++){
         for(int j = 0; j != N; j ++){
-            cout << matrix[i][j] + '0' << '\t';
+            cout << matrix[i][j] << '\t';
         }
         cout << endl;
     }
+
+	cout << endl;
 
     rotate(*matrix, 5);
 
     for(int i = 0; i != N; i ++){
         for(int j = 0; j != N; j ++){
-            cout << matrix[i][j] + '0' << '\t';
+            cout << matrix[i][j] << '\t';
         }
         cout << endl;
     }
@@ -36,20 +39,34 @@ void rotate(int* matrix, int n){
     int temp = 0;
     int last_x = 0;
     int last_y = 0;
+	int temp_position = 0;
 
-    for(int x = 0; x != n/2 + 1; x ++){
-        for(int y = 0; y != n/2 + 1; y ++){
+	int endpoint = (n % 2 == 0)?(n/2):(n/2 + 1);
+
+	bool visit[N*N];
+
+	memset(visit, false, sizeof(visit));
+
+    for(int x = 0; x != endpoint; x ++){
+        for(int y = 0; y != endpoint; y ++){
+			if(visit[y * n + x]){
+				break;
+			}
             temp = matrix[y * n + x];
             last_x = x;
             last_y = y;
+
+			visit[y * n + x] = true;
             
             for(int i = 0; i != 3; i ++){
                 matrix[last_y * n + last_x] = matrix[last_x * n + n - last_y - 1];
-                last_x = n - last_y - 1;
+				temp_position = last_y;
                 last_y = last_x;
+				last_x = n - temp_position - 1;
+				visit[last_y * n + last_x] = true;
             }
             
-            matrix[last_y * n + last_x] = 
+            matrix[last_y * n + last_x] = temp; 
         }
     }
 }
